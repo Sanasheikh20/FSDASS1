ae65754f16fa4c5e6777c31509867e2ddf9b6526
shana <sheikhsana2011@gmail.com>
2023-08-07 20:58:17 +0530

    first commit

---
+++ fsdass1.py
@@ -0,0 +1,12 @@
+
+def sum_digits(num):
+    sum = 0
+    while(num > 0):
+        rem = num % 10
+        sum = sum + rem
+        num = num // 10
+    return sum
+# Input --> list of positive integers
+inp = [120, 20, 42, 212, 802, 139, 175, 802, 468]
+print("Input List = ", inp)
+print("Maximum Value in the List = ", max(inp, key = sum_digits))
