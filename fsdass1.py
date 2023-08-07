278bf86b248c2a101a71079a4c1c9abc704f9aab
Sanasheikh20 <92053512+Sanasheikh20@users.noreply.github.com>
2023-08-07 21:57:54 +0530

    Update fsdass1.py

--- fsdass1.py
+++ fsdass1.py
@@ -7,7 +7,6 @@ def sum_digits(num):
         num = num // 10
     return sum
 
-
 inp = [120, 20, 42, 212, 802, 139, 175, 802, 468]
 print("Input List = ", inp)
 print("Maximum Value in the List = ", max(inp, key = sum_digits))
